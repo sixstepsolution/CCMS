523100ffa0d282937266303df5a5eb047b7d140c
Kanna <kannavenkateswarlu29@gmail.com>
2021-06-21 12:04:40 +0530

    Receive Inbond call completed

--- CCMS/AngularJs/controllers/ReceiveInbondCntrl.js
+++ CCMS/AngularJs/controllers/ReceiveInbondCntrl.js
@@ -3,33 +3,56 @@
     $scope.pdfModel = "";
     $scope.ShowIframe = false;
     $scope.showNewCall = false;
-
+    $scope.IsBack = false;
+    $scope.IsData = false;
+    
     $scope.SearchbyPerson = function () {
+        $scope.showNewCall = false;
+        $scope.ShowPersonDetails = false;
+        $scope.showReceiveInbondCall = false;
         $scope.IsShowSearchdata = true;
+        $scope.IsBack = false;
+        $scope.IsData = false;
+
     }
 
-    $scope.ShowPersonDetails1 = function () {
+    $scope.SearchDetailsPerson = function () {
         $scope.ShowPersonDetails = true;
         $scope.IsShowSearchdata = false;
+        $scope.showReceiveInbondCall = false;
         $scope.showNewCall = false;
+        $scope.IsBack = true;
+        $scope.IsData = true;
 
     }
 
     $scope.NewReciveInbondCall = function () {
         $scope.showNewCall = true;
         $scope.ShowPersonDetails = false;
+        $scope.showReceiveInbondCall = false;
         $scope.IsShowSearchdata = false;
+        $scope.IsBack = true;
+        $scope.IsData = false;
     }
 
     $scope.BacltoList = function () {
         $scope.showNewCall = false;
+        $scope.showReceiveInbondCall = false;
         $scope.ShowPersonDetails = false;
         $scope.IsShowSearchdata = false;
+        $scope.IsBack = false;
+        $scope.IsData = false;
     }
-    $scope.showDocument = function () {
-        //$scope.ShowIframe = false;
-        $timeout(function () {
-            $scope.ShowIframe = true;
-        }, 2000);
+    $scope.ShowReceiveIbondCalls = function () {
+        $scope.showNewCall = false;
+        $scope.showReceiveInbondCall = true;
+        $scope.ShowPersonDetails = false;
+        $scope.IsShowSearchdata = false;
+        $scope.IsBack = true;
+        $scope.IsData = true;
+    }
+    
+    $scope.addModel = function (val) {
+        $scope.modelValue = val;
     }
 });
